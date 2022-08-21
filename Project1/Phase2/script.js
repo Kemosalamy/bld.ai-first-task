4cdc7d309cc4bebc2984745a5a7e77449aa6b7b8
mohanedashraf2014@gmail.com <mohaned.elhaddad@bld.ai>
2022-08-21 14:54:32 +0200

    Format code with prettier

--- Project1/Phase2/script.js
+++ Project1/Phase2/script.js
@@ -1,63 +1,73 @@
-fetch('http://localhost:3000/courses')
-.then(res => res.json())
-.then((json) => {
-
+fetch("http://localhost:3000/courses")
+  .then((res) => res.json())
+  .then((json) => {
     courseDiv = document.getElementById("course-list");
     Object.entries(json).forEach(([key, value]) => {
+      let title = value["title"];
+      let image = value["image"];
+      let author = value["author"];
+      let price = value["price"];
+
+      let CourseHTML =
+        `
+        <div class="course-element">
+            <img width="240" height="135" alt="Course Logo" src="` +
+        image +
+        `">
+            <br>
+            <label style="font-weight: bold; font-size: 18px">` +
+        title +
+        `</label>
+            <label style="font-weight: lighter">` +
+        author +
+        `</label>
+            <label style="font-weight: bold; font-size: 15px">` +
+        price +
+        `</label>
+        </div>
+        `;
+
+      courseDiv.innerHTML += CourseHTML;
+    });
+  });
+
+const searchButtonPressed = () => {
+  let search = document.getElementById("search-bar").value;
+  courseDiv = document.getElementById("course-list");
+  courseDiv.innerHTML = "";
+
+  fetch("http://localhost:3000/courses")
+    .then((res) => res.json())
+    .then((json) => {
+      courseDiv = document.getElementById("course-list");
+      Object.entries(json).forEach(([key, value]) => {
         let title = value["title"];
         let image = value["image"];
         let author = value["author"];
         let price = value["price"];
 
-        let CourseHTML = `
-        <div class="course-element">
-            <img width="240" height="135" alt="Course Logo" src="` + image + `">
-            <br>
-            <label style="font-weight: bold; font-size: 18px">` + title + `</label>
-            <label style="font-weight: lighter">` + author + `</label>
-            <label style="font-weight: bold; font-size: 15px">` + price + `</label>
-        </div>
-        `;
-
-        courseDiv.innerHTML += CourseHTML;
-      })
-
-})
-
-
-const searchButtonPressed = () => {
-    let search = document.getElementById("search-bar").value;
-    courseDiv = document.getElementById("course-list");
-    courseDiv.innerHTML = ""
-
-    fetch('http://localhost:3000/courses')
-    .then(res => res.json())
-    .then((json) => {
-
-        courseDiv = document.getElementById("course-list");
-        Object.entries(json).forEach(([key, value]) => {
-            let title = value["title"];
-            let image = value["image"];
-            let author = value["author"];
-            let price = value["price"];
-
-            let CourseHTML = `
+        let CourseHTML =
+          `
             <div class="course-element">
-                <img width="240" height="135" alt="Course Logo" src="` + image + `">
+                <img width="240" height="135" alt="Course Logo" src="` +
+          image +
+          `">
                 <br>
-                <label style="font-weight: bold; font-size: 18px">` + title + `</label>
-                <label style="font-weight: lighter">` + author + `</label>
-                <label style="font-weight: bold; font-size: 15px">` + price + `</label>
+                <label style="font-weight: bold; font-size: 18px">` +
+          title +
+          `</label>
+                <label style="font-weight: lighter">` +
+          author +
+          `</label>
+                <label style="font-weight: bold; font-size: 15px">` +
+          price +
+          `</label>
             </div>
             `;
 
-            if(title.toLowerCase().includes(search.toLowerCase())){
-                courseDiv.innerHTML += CourseHTML;
-            }
-
-
-          })
-
-    })
-}
-
+        if (title.toLowerCase().includes(search.toLowerCase())) {
+          courseDiv.innerHTML += CourseHTML;
+        }
+      });
+    });
+};
